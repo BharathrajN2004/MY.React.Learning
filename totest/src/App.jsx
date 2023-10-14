187cda56536418add9c8cdb916aec344f11f5f11
bharathraj <97496230+Bharathrajskillpro@users.noreply.github.com>
2023-10-14 12:52:08 +0530

    completed

---
+++ totest/src/App.jsx
@@ -0,0 +1,10 @@
+import { H1, H2, H3 } from "./H1";
+
+export default function App() {
+  return (<>
+    <H1 />
+    <H2 />
+    <H3 />
+  </>);
+}
+
---
+++ totest/src/H1.jsx
@@ -0,0 +1,14 @@
+
+
+export const H1 = () => {
+    return (<h1>hello</h1>)
+}
+export const H2 = () => {
+    return (<h2>hello</h2>)
+}
+
+export const H3 = () => {
+    return (<h3>hello</h3>)
+}
+
+
